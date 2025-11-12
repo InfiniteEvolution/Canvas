811a6020380c598a1286a57080faf3ac526a10e5
InfiniteEvolution <sijo.perumadan@gmail.com>
2025-11-12 23:24:01 +0530

    Initial Commit

---
+++ Canvas/CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasApp.swift
+//  Canvas
+//
+//  Created by sijo on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
